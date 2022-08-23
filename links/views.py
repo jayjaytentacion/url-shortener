842d92fafaa4c588dea07b6c0750b63802a9511c
okputu julius <juliusstan10@gmail.com>
2022-08-23 23:24:21 +0100

    add:heroku set up,refactor:views

--- links/views.py
+++ links/views.py
@@ -8,6 +8,7 @@ import pyshorteners
 from rest_framework import status
 from .utils import generate_random_id
 from django.shortcuts import redirect
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -19,24 +20,41 @@ class Linkshortener(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         long_url=self.request.data['long_url']
-        queryset=Link.objects.filter(long_url=long_url)
-        if queryset.exists():
-            short_url=queryset[0].short_url
-            return Response({'long_url':long_url,'short_url':short_url})
-        else:
-            short_id=generate_random_id()
-            short_url = 'http://127.0.0.1:8000/'+ short_id
-            print(short_url)
-        return self.create(request,short_url,*args, **kwargs)  
-
+        try:
+            #checks if the long link already exists in the database
+           set=Link.objects.get(long_url=long_url)
+        #if the link does not exist 
+        # it creates a new short id   
+        except :
+                def gen_short_id():
+                    short_url=generate_random_id()
+                    try:
+                        #checks if the short id exists
+                        Link.objects.get(short_url=short_url)
+                    except :
+                        return short_url
+                    #makes sure id's are unique by recursion
+                    return gen_short_id() 
+                #returns the unique id to a variable    
+                short_url = gen_short_id()
+                #sends a request to create a new entry
+                return self.create(request,short_url,*args, **kwargs) 
+        #if the long link already exists,appends the domain and returns a response
+        short_url='http://j-links.herokuapp.com'+set[0].short_url
+        return Response({'long_url':long_url,'short_url':short_url})
 
 
     def create(self, request,short_url, *args, **kwargs):
+        """
+        if the posted link does not exist,creates a new entry with a unique 
+        short id
+        """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer,short_url)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response({'long_url':serializer.data['long_url'],
+        'short_url':'http://j-links.herokuapp.com'+serializer.data['short_url']}, status=status.HTTP_201_CREATED, headers=headers)
 
 
     def perform_create(self, serializer,short_url):
@@ -45,8 +63,13 @@ class Linkshortener(CreateAPIView):
 
 
 def redirect_view(request,short_url):
-    short_url='http://127.0.0.1:8000/'+short_url
-    obj = Link.objects.get(short_url=short_url)
-  
+    """
+    when this view is invoked it redirects to the long url in the database
+    """
+    try:
+       obj = Link.objects.get(short_url=short_url)
+    except :
+        return  HttpResponse('does not exist')
     if obj is not None:
-        return redirect(obj.long_url)        
+        return redirect(obj.long_url)  
+           
--- url_short/settings.py
+++ url_short/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!tudj%j8m&d1z*w^(cylot5srw!a87@nky=bh+znw7)7^u9v$+'
+SECRET_KEY = config('SECRET_KEY',default='django-insecure-!tudj%j8m&d1z*w^(cylot5srw!a87@nky=bh+znw7)7^u9v$+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'url_short.urls'
@@ -74,13 +76,19 @@ WSGI_APPLICATION = 'url_short.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASS'),
+        'HOST': 'ec2-3-224-184-9.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+import dj_database_url
+database_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(database_from_env)
 
 
 # Password validation
@@ -117,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
